2ba8572d294e219488f135643625420359fb3902
Thien Tran <19trant7@gmail.com>
2021-03-26 22:07:55 -0500

    Added new ability to click buttons using the headset lever

---
+++ Assets/Bluetooth Button/clickButtonMR.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clickButtonMR : MonoBehaviour
+{
+
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Click button when user "clicks" using the side lever
+        if (Input.GetButtonUp("Fire1"))
+        {
+            button.onClick.Invoke();
+            Debug.Log("Fire 1 Pressed");
+        }
+    }
+}

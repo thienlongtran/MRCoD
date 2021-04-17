e189f0ebcd70f26a5e4b61f516bd4013e5db68bb
Thien Tran <19trant7@gmail.com>
2021-04-17 13:25:47 -0500

    Added virtual keyboard to CreateWord scene

---
+++ Assets/Scripts/Mode 2/Word/StartKeyboard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRKeyboard.Utils;
+
+public class StartKeyboard : MonoBehaviour
+{
+
+    [SerializeField]
+    InputField thisInput;
+
+    [SerializeField]
+    KeyboardManager keyboard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Outputs keyboard inputs to current InputField
+    public void setKeyboardField()
+    {
+        keyboard.setCurrentInputField(thisInput);
+    }
+}
